9e7186e40084137eaab4081497ccb180d8e83db0
DeyaSHTA93 <93060098+DeyaSHTA93@users.noreply.github.com>
2022-01-23 16:46:55 +0100

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        Deya's Dictionary App
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          Deya's Dictionary App
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Deya Shrestha</footer>
+      </div>
     </div>
   );
 }
